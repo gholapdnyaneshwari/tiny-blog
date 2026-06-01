c57b567c2c5aed254358a3c4d4f2c9fbdef32474
gholapdnyaneshwari <gholapdnyaneshwari31@gmail.com>
2026-06-01 15:22:50 +0530

    changes added

--- server/index.js
+++ server/index.js
@@ -9,22 +9,48 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let requestCount = 0;
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, );
-            if (conn) {
-                console.log("MongoDB connected");
-            }
+        const conn = await mongoose.connect(process.env.MONGO_URI,);
+        if (conn) {
+            console.log("MongoDB connected");
+        }
     } catch (error) {
         console.error("MongoDB connection error:", error);
     }
 };
 
-app.get('/', (req, res) => {
-    res.json({ 
+app.get("/api/request-count", (req, res) => {
+    res.json({ requestCount });
+});
+
+app.use((req, res, next) => {
+    requestCount++;
+    next();
+});
+app.get("/", (req, res) => {
+    res.json({
         success: true,
         message: "server is running",
     });
+});
+
+const checkHeaderkey = (req, res, next) => {
+    const { api_token } = req.headers;
+    console.log("Checking API token:", api_token);
+
+};
+app.get("/api/test1", (req, res) => {
+        console.log("Actual Controller test1 called");
+        res.json({ message: "Test1 route reached" });
+    }
+    )
+
+app.get("/api/test2", (req, res) => {
+    console.log("Actual Controller test2 called");
+    res.json({ message: "Test2 route reached" });
 });
 
 
